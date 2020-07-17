c9a319f714058cdf1b8ba541c6284741e073b4b4
Serhat Teker <serhat.teker@gmail.com>
2020-07-17 19:36:06 +0300

    Add news urls in core urls

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("users/", include("djangonews.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("", include('news.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
