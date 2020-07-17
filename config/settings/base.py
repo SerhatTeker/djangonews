ac4064c9dea5e71967485c3dff197c9171a1ced8
Serhat Teker <serhat.teker@gmail.com>
2020-07-17 18:43:38 +0300

    Add news app

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,6 +70,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "djangonews.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "djangonews.news.apps.NewsConfig"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
